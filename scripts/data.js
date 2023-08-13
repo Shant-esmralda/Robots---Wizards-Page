77d47b44c1092805aedcd37ec8d7c46bdcf8fcac
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2023-08-13 13:55:01 +0330

    added tracks and folders started js

---
+++ scripts/data.js
@@ -0,0 +1,17 @@
+let songs = [
+    {
+        name: 'Jungle Level',
+        path: 'assets/music/Jungle Level.mp3',
+        cover: 'assets/img/jungle level.jpg'
+    },
+    {
+        name: 'Underground Bunker',
+        path: 'assets/music/Underground Bunker.mp3',
+        cover: 'assets/img/underground bunker.jpg'
+    },
+    {
+        name: 'Cacodemon Invasion',
+        path: 'assets/music/Cacodemon Invasion.mp3',
+        cover: 'assets/img/cacodemon invasion.jpg'
+    },
+]
